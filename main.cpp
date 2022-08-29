a21de56fc60efc77969d470e8846c9b3bfb193ff
Julian Mangott <julian.mangott@uibk.ac.at>
2022-08-29 16:01:20 +0200

    Remove functor class

--- main.cpp
+++ main.cpp
@@ -71,92 +71,48 @@ double system_ep::rule<0>(vector<double> y)
 // MY CLASSES //
 ////////////////
 
-class sys
+class myreact;
+
+class mysys
 {
 public:
     vector<double> &x;
     vector<string> species_names;
-    sys(vector<double> &_x, vector<string> _names);
-    // static set<react *> react_instances;
-
-    template <class T>
-    double propensity(T &prop_functor)
-    {
-        return prop_functor(x);
-    }
-};
-
-sys::sys(vector<double> &_x, vector<string> _names) : x(_x), species_names(_names) {}
-
-class react
-{
-public:
-    vector<int> population_change;
-    // double propensity();
-    react(vector<int> _nu, double (*_prop)(vector<double>)) : population_change(_nu), prop(_prop)
-    {
-        // reference_sys.react_instances.insert(this);
-    }
-    virtual ~react()
-    {
-        // reference_sys.react_instances.erase(this);
-    }
-    double operator()(const vector<double> x)
-    {
-        return prop(x);
-    }
-
-private:
-    double (*prop)(vector<double>);
-};
-
-
-/////////////////////////
-// ALTERNATIVE CLASSES //
-/////////////////////////
-
-class react1;
-
-class sys1
-{
-public:
-    vector<double> &x;
-    vector<string> species_names;
-    sys1(vector<double> &_x, vector<string> _names);
-    vector<react1*> reactions;
+    mysys(vector<double> &_x, vector<string> _names);
+    vector<myreact*> reactions;
     size_t mu();
 };
 
-sys1::sys1(vector<double> &_x, vector<string> _names) : x(_x), species_names(_names) {}
+mysys::mysys(vector<double> &_x, vector<string> _names) : x(_x), species_names(_names) {}
 
-size_t sys1::mu()
+size_t mysys::mu()
 {
     return reactions.size();
 }
 
-class react1
+class myreact
 {
 public:
     vector<int> nu;
     double propensity();
-    react1(sys1& _ref_system, vector<int> _nu, double (*_prop_function)(vector<double>));
-    virtual ~react1();
+    myreact(mysys& _ref_system, vector<int> _nu, double (*_prop_function)(vector<double>));
+    virtual ~myreact();
 private:
     double (*prop_function)(vector<double>);
-    sys1& ref_system;
+    mysys& ref_system;
 };
 
-react1::react1(sys1& _ref_system, vector<int> _nu, double (*_prop_function)(vector<double>)) : ref_system(_ref_system), nu(_nu), prop_function(_prop_function)
+myreact::myreact(mysys& _ref_system, vector<int> _nu, double (*_prop_function)(vector<double>)) : ref_system(_ref_system), nu(_nu), prop_function(_prop_function)
 {
     ref_system.reactions.push_back(this);
 }
 
-react1::~react1()
+myreact::~myreact()
 {
     ref_system.reactions.erase(std::remove(ref_system.reactions.begin(), ref_system.reactions.end(), this), ref_system.reactions.end());
 }
 
-double react1::propensity()
+double myreact::propensity()
 {
     return prop_function(ref_system.x);
 }
@@ -186,19 +142,14 @@ int main()
     vector<string> nn = {"a", "b", "c", "d"};
     vector<int> nunu = {0, 0, 0, 0};
 
-    sys mysys(xx, nn);
-    react myreact0(nunu, prop0);
-    // react myreact1(nunu, [](vector<double> y){return y[0] * y[1];});
-
-    sys1 mysys1(xx, nn);
-    react1 myreact10(mysys1, nunu, prop0);
+    mysys mysys1(xx, nn);
+    myreact myreact10(mysys1, nunu, prop0);
     // react1 myreact11(mysys1, nunu, [](vector<double> y){ return y[0] * y[1]; });
 
     system_ep mysys2;
 
     int64_t d1 = 0;
     int64_t d2 = 0;
-    int64_t d3 = 0;
 
     for (size_t ii = 0; ii < runs; ii++)
     {
@@ -206,7 +157,7 @@ int main()
         for (size_t i = 0; i < max_it; i++)
         {
             xx[0] = i;
-            mysys.propensity(myreact0);
+            mysys1.reactions[0]->propensity();
         }
         auto stop1 = high_resolution_clock::now();
         auto duration1 = duration_cast<microseconds>(stop1 - start1);
@@ -216,21 +167,11 @@ int main()
         for (size_t i = 0; i < max_it; i++)
         {
             xx[0] = i;
-            mysys1.reactions[0]->propensity();
+            mysys2.rule<0>(xx);
         }
         auto stop2 = high_resolution_clock::now();
         auto duration2 = duration_cast<microseconds>(stop2 - start2);
         d2 += duration2.count();
-
-        auto start3 = high_resolution_clock::now();
-        for (size_t i = 0; i < max_it; i++)
-        {
-            xx[0] = i;
-            mysys2.rule<0>(xx);
-        }
-        auto stop3 = high_resolution_clock::now();
-        auto duration3 = duration_cast<microseconds>(stop3 - start3);
-        d3 += duration3.count();
 
         // for (auto it : mysys1.reactions)
         // {
@@ -239,9 +180,8 @@ int main()
 
     }
 
-    cout << "Time taken by functor class: " << d1 << " microseconds." << endl;
-    cout << "Time taken by my class: " << d2 << " microseconds." << endl;
-    cout << "Time taken by system_ep: " << d3 << " microseconds." << endl;
+    cout << "Time taken by my class: " << d1 << " microseconds." << endl;
+    cout << "Time taken by system_ep: " << d2 << " microseconds." << endl;
 
     return 0;
 }
