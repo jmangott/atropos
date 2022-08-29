2ae12426aec9d52673b5eedc64b403502b21818b
Julian Mangott <julian.mangott@uibk.ac.at>
2022-08-29 15:57:04 +0200

    Add templates from `low_rank_cme`

--- main.cpp
+++ main.cpp
@@ -12,6 +12,65 @@ using std::function;
 using std::string;
 using std::vector;
 
+typedef long ind;
+
+//////////////////////////
+// LOW_RANK_CME OBJECTS //
+//////////////////////////
+
+#define RULE_SET(__name, __d, ...)            \
+    struct __name                             \
+    {                                         \
+        template <ind i>                      \
+        static double rule(vector<double> x); \
+                                              \
+        template <ind i>                      \
+        vector<ind> static depends_on();      \
+                                              \
+        static constexpr size_t d = (__d);    \
+                                              \
+        static vector<string> names()         \
+        {                                     \
+            return {__VA_ARGS__};             \
+        }                                     \
+    }
+
+#define RULE(__name, __i, __rule, ...)              \
+    template <>                                     \
+    double __name::rule<(__i)>(vector<__name::d> x) \
+    {                                               \
+        return (__rule);                            \
+    }                                               \
+    template <>                                     \
+    vector<ind> __name::depends_on<(__i)>()         \
+    {                                               \
+        return {__VA_ARGS__};                       \
+    }
+
+struct system_ep
+{
+    template <ind i>
+    static double rule(vector<double> y);
+
+    template <ind i>
+    vector<ind> static depends_on();
+
+    static vector<std::string> names()
+    {
+        return {"a", "b", "c", "d"};
+    }
+};
+
+template <>
+double system_ep::rule<0>(vector<double> y)
+{
+    return y[0] * y[1];
+}
+
+////////////////
+// MY CLASSES //
+////////////////
+
 class sys
 {
 public:
@@ -50,6 +109,7 @@ public:
 private:
     double (*prop)(vector<double>);
 };
+
 
 /////////////////////////
 // ALTERNATIVE CLASSES //
@@ -101,6 +161,7 @@ double react1::propensity()
     return prop_function(ref_system.x);
 }
 
+
 //////////////////
 // PROPENSITIES //
 //////////////////
@@ -110,57 +171,77 @@ double prop0(vector<double> y)
     return y[0] * y[1];
 }
 
+
+////////////////////////////////////////////
+// COMPARISON OF THE DIFFERENT APPROACHES //
+////////////////////////////////////////////
+
 int main()
 {
+    int runs = 20;
+    int max_it = 100000;
+
+
     vector<double> xx = {1, 2, 3, 4};
     vector<string> nn = {"a", "b", "c", "d"};
     vector<int> nunu = {0, 0, 0, 0};
 
     sys mysys(xx, nn);
     react myreact0(nunu, prop0);
-    react myreact1(nunu, [](vector<double> y){return y[0] * y[1];});
+    // react myreact1(nunu, [](vector<double> y){return y[0] * y[1];});
 
     sys1 mysys1(xx, nn);
     react1 myreact10(mysys1, nunu, prop0);
-    react1 myreact11(mysys1, nunu, [](vector<double> y){ return y[0] * y[1]; });
+    // react1 myreact11(mysys1, nunu, [](vector<double> y){ return y[0] * y[1]; });
 
-    auto start1 = high_resolution_clock::now();
-    for (size_t i = 0; i < 1000; i++)
+    system_ep mysys2;
+
+    int64_t d1 = 0;
+    int64_t d2 = 0;
+    int64_t d3 = 0;
+
+    for (size_t ii = 0; ii < runs; ii++)
     {
-        xx[0] = i;
-        mysys.propensity(myreact0);
+        auto start1 = high_resolution_clock::now();
+        for (size_t i = 0; i < max_it; i++)
+        {
+            xx[0] = i;
+            mysys.propensity(myreact0);
+        }
+        auto stop1 = high_resolution_clock::now();
+        auto duration1 = duration_cast<microseconds>(stop1 - start1);
+        d1 += duration1.count();
+
+        auto start2 = high_resolution_clock::now();
+        for (size_t i = 0; i < max_it; i++)
+        {
+            xx[0] = i;
+            mysys1.reactions[0]->propensity();
+        }
+        auto stop2 = high_resolution_clock::now();
+        auto duration2 = duration_cast<microseconds>(stop2 - start2);
+        d2 += duration2.count();
+
+        auto start3 = high_resolution_clock::now();
+        for (size_t i = 0; i < max_it; i++)
+        {
+            xx[0] = i;
+            mysys2.rule<0>(xx);
+        }
+        auto stop3 = high_resolution_clock::now();
+        auto duration3 = duration_cast<microseconds>(stop3 - start3);
+        d3 += duration3.count();
+
+        // for (auto it : mysys1.reactions)
+        // {
+        //     cout << it->propensity() << endl;
+        // }
+
     }
-    auto stop1 = high_resolution_clock::now();
-    auto duration1 = duration_cast<microseconds>(stop1 - start1);
 
-    cout << "Time taken by loop 1:" << duration1.count() << " microseconds" << endl;
-
-    auto start2 = high_resolution_clock::now();
-    for (size_t i = 0; i < 1000; i++)
-    {
-        xx[0] = i;
-        mysys.propensity(myreact1);
-    }
-    auto stop2 = high_resolution_clock::now();
-    auto duration2 = duration_cast<microseconds>(stop2 - start2);
-
-    cout << "Time taken by loop 2:" << duration2.count() << " microseconds" << endl;
-
-    auto start3 = high_resolution_clock::now();
-    for (size_t i = 0; i < 1000; i++)
-    {
-        xx[0] = i;
-        mysys1.reactions[1]->propensity();
-    }
-    auto stop3 = high_resolution_clock::now();
-    auto duration3 = duration_cast<microseconds>(stop3 - start3);
-
-    cout << "Time taken by loop 3:" << duration3.count() << " microseconds" << endl;
-
-    // for (auto it : mysys1.reactions)
-    // {
-    //     cout << it->propensity() << endl;
-    // }
+    cout << "Time taken by functor class: " << d1 << " microseconds." << endl;
+    cout << "Time taken by my class: " << d2 << " microseconds." << endl;
+    cout << "Time taken by system_ep: " << d3 << " microseconds." << endl;
 
     return 0;
 }
