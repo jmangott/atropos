4f0149a63b29b49954ddc153d2cf22ac19286440
Julian Mangott <julian.mangott@uibk.ac.at>
2022-09-11 11:01:38 +0200

    Add cmake support

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <lr/lr.hpp>
+#include <generic/matrix.hpp>
+#include <generic/storage.hpp>
+#include <lr/coefficients.hpp>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+    multi_array<double, 1> my_mat({2});
+    cout << "Hello!" << endl;
+    return 0;
+}
