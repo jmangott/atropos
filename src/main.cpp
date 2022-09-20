57dcdda7d55705288746c467cc4306f4ec0f8d32
Julian Mangott <julian.mangott@uibk.ac.at>
2022-09-20 10:18:35 +0200

    Add reaction and system classes

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <vector>
 
-#include <lr/lr.hpp>
 #include <generic/matrix.hpp>
 #include <generic/storage.hpp>
 #include <lr/coefficients.hpp>
+#include <lr/lr.hpp>
+
+#include "reactions.hpp"
 
 using std::cout;
 using std::endl;
@@ -14,7 +16,13 @@ using std::vector;
 
 int main()
 {
+    vector<double> xx = {1, 2, 3, 4};
+    vector<string> nn = {"a", "b", "c", "d"};
+    vector<int> nunu = {0, 0, 0, 0};
+    mysys mysys1(xx, nn);
+    myreact myreact1(nunu, [](vector<double> y) { return y[0] * y[1]; }, mysys1);
     multi_array<double, 1> my_mat({2});
     cout << "Hello!" << endl;
+
     return 0;
 }
---
+++ src/reactions.cpp
@@ -0,0 +1,26 @@
+#include "reactions.hpp"
+
+using std::vector;
+using std::string;
+
+mysys::mysys(vector<double> &_x, vector<string> _names) : x(_x), species_names(_names) {}
+
+size_t mysys::mu()
+{
+    return reactions.size();
+}
+
+myreact::myreact(vector<int> _nu, double (*_prop_function)(vector<double>), mysys &_ref_system) : nu(_nu), prop_function(_prop_function), ref_system(_ref_system)
+{
+    ref_system.reactions.push_back(this);
+}
+
+myreact::~myreact()
+{
+    ref_system.reactions.erase(std::remove(ref_system.reactions.begin(), ref_system.reactions.end(), this), ref_system.reactions.end());
+}
+
+double myreact::propensity()
+{
+    return prop_function(ref_system.x);
+}
