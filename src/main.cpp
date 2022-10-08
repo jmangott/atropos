d9a334c245b9c454b71df85eeec53c3a3ecc0081
Julian Mangott <julian.mangott@uibk.ac.at>
2022-10-08 23:09:43 +0200

    Add `CalculateWeightX2` and `ShiftMultiArrayRows`

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,10 @@ using std::stringstream;
 using std::string;
 using std::vector;
 
-// TODO: improve speed by using a lookup table?
-vector<double> index_to_state(Index idx, vector<Index> interval)
+// TODO: Improve speed by using a lookup table (?)
+
+// Convert combined index to vector containing the population numbers
+vector<double> IndexToState(Index idx, vector<Index> interval)
 {
     int stride = 1;
     size_t dim = interval.size();
@@ -40,45 +42,111 @@ vector<double> index_to_state(Index idx, vector<Index> interval)
     return state_vec;
 }
 
+
+// Calculate the integration weight for coefficients C2 and D2 (depending on `alpha1` and `mu`)
+multi_array<double, 1> CalculateWeightX2(int d, int m1, int m2, vector<Index> n_xx1, vector<Index> n_xx2, Index dxx2_mult, vector<double> h_xx2, int alpha1, int mu)
+{
+    vector<double> a_vec(dxx2_mult, 0);
+    vector<double> n_tot(d, 0);
+    vector<double> n1(m1, 0);
+    vector<double> n2(m2, 0);
+    multi_array<double, 1> w_x2({dxx2_mult});
+
+    n1 = IndexToState(alpha1, n_xx1);
+    for (size_t i = 0; i < n1.size(); i++)
+        n_tot[i] = n1[i];
+
+    for (int alpha2 = 0; alpha2 < dxx2_mult; alpha2++)
+    {
+        n2 = IndexToState(alpha2, n_xx2);
+        for (size_t i = 0; i < n2.size(); i++)
+            n_tot[m1 + i] = n1[i];
+        a_vec[alpha2] = mysystem.reactions[mu]->propensity(n_tot);
+    }
+
+    double h_xx2_mult = 1;
+    for (auto &ele : h_xx2)
+        h_xx2_mult *= ele;
+    for (size_t i = 0; i < a_vec.size(); i++)
+        w_x2(i) = a_vec[i] * h_xx2_mult;
+
+    return w_x2;
+}
+
+
+// TODO: Change to void ..., pass output by reference
+// Calculate array where rows are shifted by index_shift
+multi_array<double, 2> ShiftMultiArrayRows(int r, Index dxx2_mult, vector<int> k_xx2, vector<const double *> &input_pointer, int index_shift)
+{
+    vector<const double *> pointer_shift = input_pointer;
+    for (int i = 0; i < r; i++)
+        pointer_shift[i] += int(k_xx2[0] * index_shift);
+
+    multi_array<double, 2> array_shift({dxx2_mult, r});
+    for (int j = 0; j < r; j++)
+    {
+        for (Index i = 0; i < dxx2_mult; i++)
+        {
+            if ((k_xx2[0] * index_shift < 0) &&
+                (i < -k_xx2[0] * index_shift))
+            {
+                array_shift(i, j) = input_pointer[j][0];
+            }
+            else if ((k_xx2[0] * index_shift > 0) &&
+                     (i >= (dxx2_mult - k_xx2[0] * index_shift)))
+            {
+                array_shift(i, j) = input_pointer[j][dxx2_mult - 1];
+            }
+            else
+            {
+                array_shift(i, j) = pointer_shift[j][i];
+            }
+        }
+    }
+
+    return array_shift;
+}
+
+
 int main()
 {
     /////////////////////////////////////////////
     ////// DECLARATION AND INITIALIZATION ///////
     /////////////////////////////////////////////
 
-    constexpr int r = 5; // rank
-    constexpr int d = 2; // # species
+    constexpr int kR = 5; // rank
+    constexpr int kD = 2; // # species
  
-    std::fill_n(std::back_inserter(xx), d, 1.0);
+    std::fill_n(std::back_inserter(xx), kD, 1.0);
     nn = {"A1", "B2"};
 
     // Index nsteps = 1000; // # time steps
     // double tstar = 1.0; // final time
     // double tau = tstar / nsteps; // time step size
 
-    constexpr size_t m1 = d / 2;
-    constexpr size_t m2 = d - m1;
+    constexpr size_t kM1 = kD / 2;
+    constexpr size_t kM2 = kD - kM1;
 
     // Number of grid points for each species in partition 1 and 2
-    vector<Index> n_xx1(m1, 51);
-    vector<Index> n_xx2(m2, 51);
+    vector<Index> n_xx1(kM1, 51);
+    vector<Index> n_xx2(kM2, 51);
 
     // Limits for the population number in partition 1 and 2
-    vector<int> lim_xx1(m1, 50);
-    vector<int> lim_xx2(m2 ,50);
+    vector<int> lim_xx1(kM1, 50);
+    vector<int> lim_xx2(kM2 ,50);
 
     // Initial datum generation
-    vector<double> h_xx1(m1, 0);
-    vector<double> h_xx2(m2, 0);
-    vector<int> k_xx1(m1, 0);
-    vector<int> k_xx2(m2, 0);
-    for (size_t ii = 0; ii < m1; ii++)
+    vector<double> h_xx1(kM1, 0);
+    vector<double> h_xx2(kM2, 0);
+    vector<int> k_xx1(kM1, 0);
+    vector<int> k_xx2(kM2, 0);
+    for (size_t ii = 0; ii < kM1; ii++)
     {
         h_xx1[ii] = double(lim_xx1[ii]) / (n_xx1[ii] - 1);
         k_xx1[ii] = int((n_xx1[ii] - 1) / lim_xx1[ii]);
     }
 
-    for (size_t ii = 0; ii < m2; ii++)
+    for (size_t ii = 0; ii < kM2; ii++)
     {
         h_xx2[ii] = lim_xx2[ii] / (n_xx2[ii] - 1);
         k_xx2[ii] = int((n_xx2[ii] - 1) / lim_xx2[ii]);
@@ -86,24 +154,26 @@ int main()
 
     Index dxx1_mult = 1;
     Index dxx2_mult = 1;
-    for (auto ele : n_xx1) dxx1_mult *= ele;
-    for (auto ele : n_xx2) dxx2_mult *= ele;
+    for (auto ele : n_xx1)
+        dxx1_mult *= ele;
+    for (auto ele : n_xx2)
+        dxx2_mult *= ele;
 
     vector<const double *> x1, x2;
 
     // For coefficients
-    multi_array<double, 2> c2({r, r});
-    multi_array<double, 2> d2({r, r});
+    multi_array<double, 2> c2({kR, kR});
+    multi_array<double, 2> d2({kR, kR});
 
     multi_array<double, 1> w_x2({dxx2_mult});
 
     // Temporary objects for multiplication
-    multi_array<double, 2> tmp_x({dxx1_mult, r});
+    multi_array<double, 2> tmp_x({dxx1_mult, kR});
 
     // Set up S, X1 and X2h for t = 0
-    multi_array<double, 2> ss({r, r});
-    multi_array<double, 2> xx1({dxx1_mult, r});
-    multi_array<double, 2> xx2({dxx2_mult, r});
+    multi_array<double, 2> ss({kR, kR});
+    multi_array<double, 2> xx1({dxx1_mult, kR});
+    multi_array<double, 2> xx2({dxx2_mult, kR});
 
 
     /////////////////////////////////////////////
@@ -111,6 +181,7 @@ int main()
     /////////////////////////////////////////////
 
     // TODO: Use .netcdf instead of .csv
+    // TODO: Write one function for the reading in of the three arrays
 
     string line;
 
@@ -175,16 +246,16 @@ int main()
 
     double *it1 = xx1.begin();
     double *it2 = xx2.begin();
-    for (size_t i = 0; i < r; i++)
+    for (size_t i = 0; i < kR; i++)
     {
         x1.push_back(it1);
         x2.push_back(it2);
-        it1 += m1 * n_xx1[0];
-        it2 += m2 * n_xx2[0];
+        it1 += kM1 * n_xx1[0];
+        it2 += kM2 * n_xx2[0];
     }
     
     // Set up the low-rank structure and the inner products
-    lr2<double> lr_sol(r, {dxx1_mult, dxx2_mult});
+    lr2<double> lr_sol(kR, {dxx1_mult, dxx2_mult});
 
     std::function<double(double *, double *)> ip_xx1 = inner_product_from_const_weight(h_xx1[0], dxx1_mult);
     std::function<double(double *, double *)> ip_xx2 = inner_product_from_const_weight(h_xx2[0], dxx2_mult);
@@ -201,21 +272,18 @@ int main()
     tmp_x = lr_sol.X;
     blas.matmul(tmp_x, lr_sol.S, lr_sol.X); // lr_sol.X contains now K
     
-    vector<double> sigma1, sigma2;
-    double sigma2_sum;
+    vector<int> sigma1, sigma2;
+    int sigma2_sum;
     
     // NOTE: when the partition requires a permutation of the original order of species,
     // then also the nu vectors and similar quantities have to be permuted
 
     for(auto it : mysystem.reactions) {
         sigma2_sum = 0;
-        for (size_t i = 0; i < m2; i++)
-        {
+        for (size_t i = 0; i < kM2; i++)
             sigma2_sum =+ it->nu[i] * pow(n_xx2[i], i);
-        }
         sigma2.push_back(sigma2_sum);
     }
-
 
     // Calculate C^2 for given mu and alpha_tilde1
 
@@ -225,40 +293,13 @@ int main()
     int mu = 2;
     int alpha_tilde1 = 30;
 
-    // Calculate the shifted X1
-
-    // TODO: write this shifting procedure as a function
-    vector<const double *> x2_shift = x2;
-    for (size_t i = 0; i < r; i++)
-    {
-        x2_shift[i] += int(k_xx2[0] * sigma2[mu]);
-    }
-
-    multi_array<double, 2> xx2_shift({dxx2_mult, r});
-    for (int j = 0; j < r; j++) 
-    {
-        for (Index i = 0; i < dxx2_mult; i++)
-        {
-            if ((k_xx2[0] * sigma2[mu] < 0) && 
-                (i < -k_xx2[0] * sigma2[mu]))
-            {
-                xx2_shift(i, j) = x2[j][0];
-            }
-            else if ((k_xx2[0] * sigma2[mu] > 0) && 
-                     (i >= (dxx2_mult - k_xx2[0] * sigma2[mu])))
-            {
-                xx2_shift(i, j) = x2[j][dxx2_mult - 1];
-            }
-            else
-            {
-                xx2_shift(i, j) = x2_shift[j][i];
-            }
-        }
-    }
+    // Calculate the shifted X2
+    multi_array<double, 2> xx2_shift({dxx2_mult, kR});
+    xx2_shift = ShiftMultiArrayRows(kR, dxx2_mult, k_xx2, x2, sigma2[mu]);
 
     // for (int i = 0; i < dxx2_mult; i++)
     // {
-    //     for (int j = 0; j < r; j++)
+    //     for (int j = 0; j < kR; j++)
     //     {
     //         cout << xx2_shift(i, j) << " ";
     //     }
@@ -268,69 +309,15 @@ int main()
     vector<size_t> dep_vec, dep_vec1, dep_vec2;
     dep_vec = mysystem.reactions[0]->depends_on;
     for (auto & ele : dep_vec)
-    {
-        (ele < m1) ? dep_vec1.push_back(ele) : dep_vec2.push_back(ele); 
-    }
+        (ele < kM1) ? dep_vec1.push_back(ele) : dep_vec2.push_back(ele);
 
-    // int stride = 1;
-    vector<double> a_vec(dxx2_mult, 0);
-    vector<double> n_tot(d, 0);
-    vector<double> n1(d, 0);
-    vector<double> n2(d, 0);
+    w_x2 = CalculateWeightX2(kD, kM1, kM2, n_xx1, n_xx2, dxx2_mult, h_xx2, alpha_tilde1, mu);
 
-    n1 = index_to_state(alpha_tilde1, n_xx1);
-    for (size_t i = 0; i < n1.size(); i++) n_tot[i] = n1[i];
-
-    // for (size_t k = 0; k < m1; k++)
-    // {
-    //     if (k == (m1 - 1))
-    //     {
-    //         n_tot[k] = int(alpha_tilde1 / stride);
-    //     }
-    //     else
-    //     {
-    //         n_tot[k] = int((alpha_tilde1 % n_xx1[k]) / stride);
-    //         stride *= n_xx1[k];
-    //     }
-    // }
-
-    for (int alpha2 = 0; alpha2 < dxx2_mult; alpha2++)
-    {
-        // stride = 1;
-        // for (size_t k = 0; k < m2; k++)
-        // {
-        //     if (k == (m2 - 1))
-        //     {
-        //         n_tot[m1 + k] = int(i / stride);
-        //     }
-        //     else
-        //     {
-        //         n_tot[m1 + k] = int((i % n_xx2[k]) / stride);
-        //         stride *= n_xx2[k];
-        //     }
-        // }
-        n2 = index_to_state(alpha2, n_xx2);
-        for (size_t i = 0; i < n2.size(); i++) n_tot[m1 + i] = n1[i];
-        a_vec[alpha2] = mysystem.reactions[mu]->propensity(n_tot);
-    }
-
-    // Set up w_x2
-    double d_xx2 = 1;
-    for (auto & ele : h_xx2) d_xx2 *= ele;
-    for (size_t i = 0; i < a_vec.size(); i++)
-    {
-        w_x2(i) = a_vec[i] * d_xx2;
-    }
-
-    // Calculate coefficient c2
+    // Calculate coefficient C2
     coeff(xx2_shift, lr_sol.V, w_x2, c2, blas);
 
-
-    // Calculate coefficient d2
+    // Calculate coefficient D2
     coeff(lr_sol.V, lr_sol.V, w_x2, d2, blas);
-
-
-
 
     return 0;
 }
