7168588e126d9f0aa136de3a49d6dde18dd8816f
Julian Mangott <julian.mangott@uibk.ac.at>
2023-01-10 20:55:18 +0100

    Update tests for `ShiftMultiArrayRows`

--- tests/test_index_functions.cpp
+++ tests/test_index_functions.cpp
@@ -9,6 +9,41 @@
 
 TEST_CASE("index_functions", "[index_functions]")
 {
+    vector<string> test_names = {"S1, S2, S3"};
+    mysys test_system(test_names);
+    myreact test_react0(
+        {-1, -1, 0}, {0, 1}, [](std::vector<double> y)
+        { return y[0] * y[1]; },
+        test_system);
+    myreact test_react1(
+        {0, -1, -1}, {1, 2}, [](std::vector<double> y)
+        { return y[1] * y[2]; },
+        test_system);
+    myreact test_react2(
+        {1, 0, 0}, {1}, [](std::vector<double> y)
+        { return 1.0 / (1.0 + y[1]); },
+        test_system);
+    myreact test_react3(
+        {0, 1, 0}, {0}, [](std::vector<double> y)
+        { return 1.0 / (1.0 + y[0]); },
+        test_system);
+
+    vector<Index> sigma1, sigma2;
+    const Index m1 = 2;
+    const Index m2 = 1;
+    multi_array<Index, 1> n1({m1});
+    multi_array<Index, 1> k1({m1});
+    multi_array<Index, 1> n2({m2});
+    multi_array<Index, 1> k2({m2});
+    n1(0) = 4;
+    n1(1) = 3;
+    k1(0) = 1;
+    k1(1) = 1;
+    n2(0) = 2;
+    k2(0) = 1;
+    grid_info grid(m1, m2, 1, n1, n2, k1, k2);
+    CalculateShiftAmount(sigma1, sigma2, test_system, grid);
+
     SECTION("VecIndexToState")
     {
         multi_array<Index, 1> vec_index({5});
@@ -71,85 +106,100 @@ TEST_CASE("index_functions", "[index_functions]")
 
     SECTION("CalculateShiftAmount")
     {
-        vector<string> test_names = {"S1, S2, S3"};
-        mysys test_system(test_names);
-        myreact test_react0(
-            {-1, -1, 0}, {0, 1}, [](std::vector<double> y)
-            { return y[0] * y[1]; },
-            test_system);
-        myreact test_react1(
-            {0, -1, -1}, {1, 2}, [](std::vector<double> y)
-            { return y[1] * y[2]; },
-            test_system);
-        myreact test_react2(
-            {1, 0, 0}, {1}, [](std::vector<double> y)
-            { return 1.0 / (1.0 + y[1]); },
-            test_system);
-        myreact test_react3(
-            {0, 1, 0}, {0}, [](std::vector<double> y)
-            { return 1.0 / (1.0 + y[0]); },
-            test_system);
-        vector<Index> sigma1, sigma2;
         vector<Index> sigma1_comparison, sigma2_comparison;
-        
-        const Index m1 = 2;
-        const Index m2 = 1;
-        multi_array<Index, 1> n1({m1});
-        multi_array<Index, 1> k1({m1});
-        multi_array<Index, 1> n2({m2});
-        multi_array<Index, 1> k2({m2});
-        n1(0) = 4;
-        n1(1) = 3;
-        k1(0) = 1;
-        k1(1) = 2;
-        n2(0) = 2;
-        k2(0) = 1;
-        grid_info grid(m1, m2, 2, n1, n2, k1, k2);
 
-        sigma1_comparison = {-9, -8, 1, 8};
+        sigma1_comparison = {-5, -4, 1, 4};
         sigma2_comparison = {0, -1, 0, 0};
-        CalculateShiftAmount(sigma1, sigma2, test_system, grid);
         REQUIRE(bool(sigma1 == sigma1_comparison));
         REQUIRE(bool(sigma2 == sigma2_comparison));
     }
 
     SECTION("ShiftMultiArrayRows1")
     {
-        Index n_rows = 12;
-        Index n_cols = 10;
+        Index n_rows = grid.dx1;
+        Index n_cols = 1;
         multi_array<double, 2> input_array({n_rows, n_cols}), output_array({n_rows, n_cols});
         multi_array<double, 2> comparison_array({n_rows, n_cols});
 
         set_zero(input_array);
-        for (Index i = 0; i < n_cols; i++)
-            input_array(i, i) = 1.0;
+        for (Index i = 0; i < n_rows; i++)
+            input_array(i, 0) = (double) i + 1;
 
         set_zero(comparison_array);
-        for (Index i = 0; i < 5; i++)
-            comparison_array(i, 0) = 1.0;
-        for (Index i = 0; i < n_rows - 5; i++)
-            comparison_array(i + 5, i) = 1.0;
+        comparison_array(0, 0) = 6.0;
+        comparison_array(1, 0) = 7.0;
+        comparison_array(2, 0) = 8.0;
+        comparison_array(3, 0) = 0.0; // !
+        comparison_array(4, 0) = 10.0;
+        comparison_array(5, 0) = 11.0;
+        comparison_array(6, 0) = 12.0;
 
-        ShiftMultiArrayRows(output_array, input_array, 5);
+        ShiftMultiArrayRows(1, output_array, input_array, sigma1[0], test_system.reactions[0]->nu, grid, test_system);
+
         REQUIRE(bool(output_array == comparison_array));
     }
 
     SECTION("ShiftMultiArrayRows2")
     {
-        Index n_rows = 12;
-        Index n_cols = 10;
+        Index n_rows = grid.dx1;
+        Index n_cols = 2;
         multi_array<double, 2> input_array({n_rows, n_cols}), output_array({n_rows, n_cols});
         multi_array<double, 2> comparison_array({n_rows, n_cols});
 
         set_zero(input_array);
-        for (Index i = 0; i < n_cols; i++)
-            input_array(i, i) = 1.0;
+        for (Index i = 0; i < n_rows; i++)
+        {
+            input_array(i, 0) = (double) i + 1;
+            input_array(i, 1) = (double) i + 2;
+        }
 
         set_zero(comparison_array);
-        for (Index i = 5; i < n_cols; i++)
-            comparison_array(i - 5, i) = 1.0;
+        comparison_array(5, 0) = 1.0;
+        comparison_array(6, 0) = 2.0;
+        comparison_array(7, 0) = 3.0;
+        comparison_array(8, 0) = 0.0; // !
+        comparison_array(9, 0) = 5.0;
+        comparison_array(10, 0) = 6.0;
+        comparison_array(11, 0) = 7.0;
 
-        ShiftMultiArrayRows(output_array, input_array, -5);
+        comparison_array(5, 1) = 2.0;
+        comparison_array(6, 1) = 3.0;
+        comparison_array(7, 1) = 4.0;
+        comparison_array(8, 1) = 0.0; // !
+        comparison_array(9, 1) = 6.0;
+        comparison_array(10, 1) = 7.0;
+        comparison_array(11, 1) = 8.0;
+
+        Index d = grid.dx1 + grid.dx2;
+        vector<int> nu(d, 0);
+
+        // Calculate -nu for shift
+        for (Index i = 0; i < d; i++)
+            nu[i] = -test_system.reactions[0]->nu[i];
+
+        ShiftMultiArrayRows(1, output_array, input_array, -sigma1[0], nu, grid, test_system);
+
+        REQUIRE(bool(output_array == comparison_array));
+    }
+
+    SECTION("ShiftMultiArrayRows3")
+    {
+        Index n_rows = grid.dx2;
+        Index n_cols = 1;
+        multi_array<double, 2> input_array({n_rows, n_cols}), output_array({n_rows, n_cols});
+        multi_array<double, 2> comparison_array({n_rows, n_cols});
+
+        set_zero(input_array);
+        for (Index i = 0; i < n_rows; i++)
+        {
+            input_array(i, 0) = (double) i + 1;
+        }
+
+        set_zero(comparison_array);
+        comparison_array(0, 0) = 2.0;
+
+        ShiftMultiArrayRows(2, output_array, input_array, sigma2[1], test_system.reactions[1]->nu, grid, test_system);
+
         REQUIRE(bool(output_array == comparison_array));
     }
 }
