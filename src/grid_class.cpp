419a1f648cd8f0e27945cf19439181f1dcb2105f
Julian Mangott <julian.mangott@uibk.ac.at>
2023-02-16 12:16:11 +0100

    Fix wrong calculation of `limr` in `grid_class`

--- src/grid_class.cpp
+++ src/grid_class.cpp
@@ -19,7 +19,7 @@ void grid_info::grid_common_init()
         }
         else
         {
-            limr1(i) = (n1(i) - 1) / k1(i);
+            limr1(i) = (n1(i) - 1) / k1(i) + liml1(i);
             h1(i) = 1.0 / k1(i);
 
             n(i) = n1(i);
@@ -42,7 +42,7 @@ void grid_info::grid_common_init()
         }
         else
         {
-            limr2(i) = (n2(i) - 1) / k2(i);
+            limr2(i) = (n2(i) - 1) / k2(i) + liml2(i);
             h2(i) = 1.0 / k2(i);
 
             n(i + m1) = n2(i);
@@ -64,14 +64,14 @@ grid_info::grid_info(Index _m1, Index _m2, Index _r, Index _n, Index _k, vector<
     std::fill(n2.begin(), n2.end(), _n);
     std::fill(k1.begin(), k1.end(), _k);
     std::fill(k2.begin(), k2.end(), _k);
+    std::copy(_liml1.begin(), _liml1.end(), liml1.begin());
+    std::copy(_liml2.begin(), _liml2.end(), liml2.begin());
 
     grid_common_init();
 }
 
 grid_info::grid_info(Index _m1, Index _m2, Index _r, multi_array<Index, 1> _n1, multi_array<Index, 1> _n2, multi_array<Index, 1> _k1, multi_array<Index, 1> _k2, multi_array<double, 1> _liml1, multi_array<double, 1> _liml2) : m1(_m1), m2(_m2), r(_r), n1(_n1), n2(_n2), n({_m1 + _m2}), k1(_k1), k2(_k2), k({_m1 + _m2}), liml1(_liml1), liml2(_liml2), liml({_m1 + _m2}), limr1({_m1}), limr2({_m2}), limr({_m1 + _m2}), h1({_m1}), h2({_m2}), h({_m1 + _m2})
 {
-    std::copy(_liml1.begin(), _liml1.end(), liml1.begin());
-    std::copy(_liml2.begin(), _liml2.end(), liml2.begin());
     grid_common_init();
 }
 
