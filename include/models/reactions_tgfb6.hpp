d788f34044cf64959b5b3e5ed76f63f8a7fc7198
Julian Mangott <julian.mangott@uibk.ac.at>
2023-03-02 09:30:38 +0100

    Fix typo in TGFb6 model

--- include/models/reactions_tgfb6.hpp
+++ include/models/reactions_tgfb6.hpp
@@ -3,7 +3,7 @@
 
 #include "../reaction_class.hpp"
 
-const std::vector<std::string> kNN = {"S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"};
+const std::vector<std::string> kNN = {"Smad2", "pSmad2", "S6K", "pS6K", "Akt", "pAkt", "Erk", "pErk"};
 constexpr double kA0 = 5.525363366159611461e-02;
 constexpr double kA1 = 0.1;
 constexpr double kA2 = 1.421915991687823499e+01;
@@ -33,8 +33,8 @@ constexpr double kX4 = 0.0;
 mysys mysystem(kNN);
 
 myreact myreact0(
-    {-1, 1, 0, 0, 0, 0, 0, 0}, {0, 3, 5}, [](std::vector<double> y)
-    { return kA14 * kX0 * y[0] * y[5] / ((kA15 + y[0]) * (1.0 + y[3] / kA16)); },
+    {-1, 1, 0, 0, 0, 0, 0, 0}, {0, 5, 7}, [](std::vector<double> y)
+    { return kA14 * kX0 * y[0] * y[5] / ((kA15 + y[0]) * (1.0 + y[7] / kA16)); },
     mysystem);
 
 myreact myreact1(
@@ -86,60 +86,5 @@ myreact myreact10(
     {0, 0, 1, -1, 0, 0, 0, 0}, {3}, [](std::vector<double> y)
     { return kA13 * y[3]; },
     mysystem);
-
-// myreact myreact0(
-//     {-1, 1, 0, 0, 0, 0, 0, 0}, {0, 3, 5}, [](std::vector<double> y)
-//     { return kA14 * kX0 * y[0] * y[5] / ((kA15 + y[0]) * (1.0 + y[3] / kA16)); },
-//     mysystem);
-
-// myreact myreact1(
-//     {1, -1, 0, 0, 0, 0, 0, 0}, {1}, [](std::vector<double> y)
-//     { return kA0 * y[1]; },
-//     mysystem);
-
-// myreact myreact2(
-//     {0, 0, -1, 1, 0, 0, 0, 0}, {2}, [](std::vector<double> y)
-//     { return kA1 * kX1 * y[2]; },
-//     mysystem);
-
-// myreact myreact3(
-//     {0, 0, -1, 1, 0, 0, 0, 0}, {2, 5}, [](std::vector<double> y)
-//     { return kA17 * y[2] * y[5]; },
-//     mysystem);
-
-// myreact myreact4(
-//     {0, 0, -1, 1, 0, 0, 0, 0}, {2}, [](std::vector<double> y)
-//     { return kA3 * kX0 * y[2] / (kA4 + y[2] + kA4 * kX2 / kA5); },
-//     mysystem);
-
-// myreact myreact5(
-//     {0, 0, 1, -1, 0, 0, 0, 0}, {3}, [](std::vector<double> y)
-//     { return kA2 * y[3]; },
-//     mysystem);
-
-// myreact myreact6(
-//     {0, 0, 0, 0, -1, 1, 0, 0}, {4}, [](std::vector<double> y)
-//     { return kA6 * kX1 * y[4]; },
-//     mysystem);
-
-// myreact myreact7(
-//     {0, 0, 0, 0, 1, -1, 0, 0}, {5, 7}, [](std::vector<double> y)
-//     { return kA7 * y[5] * y[7]; },
-//     mysystem);
-
-// myreact myreact8(
-//     {0, 0, 0, 0, -1, 1, 0, 0}, {4}, [](std::vector<double> y)
-//     { return kA8 * kX0 * y[4] / ((kA9 + y[4]) * (1.0 + kX3 / kA9)); },
-//     mysystem);
-
-// myreact myreact9(
-//     {0, 0, 0, 0, 0, 0, -1, 1}, {5, 6}, [](std::vector<double> y)
-//     { return kA10 * y[5] * y[6] / (kA11 + y[6] + kA11 * kX4 / kA12); },
-//     mysystem);
-
-// myreact myreact10(
-//     {0, 0, 0, 0, 0, 0, 1, -1}, {7}, [](std::vector<double> y)
-//     { return kA13 * y[7]; },
-//     mysystem);
 
 #endif
