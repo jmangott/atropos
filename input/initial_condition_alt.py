6f3e1e463560c718834dc80da174d93cd1b217c0
Julian Mangott <julian.mangott@uibk.ac.at>
2023-03-08 21:25:34 +0100

    Improve efficiency for initial condition

--- input/initial_condition_alt.py
+++ input/initial_condition_alt.py
@@ -23,14 +23,23 @@ from index_functions import *
 # x1_0 = np.array([0, 15], dtype="int64")
 # x2_0 = np.array([0, 5, 10], dtype="int64")
 
-# TGFb6
-kk1 = np.array([1, 1, 1, 1])
-kk2 = np.array([1, 1, 1, 1])
-nx1 = np.array([5, 5, 151, 151])
-nx2 = np.array([26, 21, 21, 21])
+# # TGFb6
+# kk1 = np.array([1, 1, 1, 1])
+# kk2 = np.array([1, 1, 1, 1])
+# nx1 = np.array([5, 5, 151, 151])
+# nx2 = np.array([26, 21, 21, 21])
 
-x1_0 = np.array([2, 0, 20, 133], dtype="int64")
-x2_0 = np.array([16, 3, 6, 12], dtype="int64")
+# x1_0 = np.array([2, 0, 20, 133], dtype="int64")
+# x2_0 = np.array([16, 3, 6, 12], dtype="int64")
+
+# Tyson
+kk1 = np.array([1, 1])
+kk2 = np.array([1, 1, 1])
+nx1 = np.array([1521, 1901])
+nx2 = np.array([16, 2041, 1251])
+
+x1_0 = np.array([1505, 0], dtype="int64")
+x2_0 = np.array([0, 2022, 0], dtype="int64")
 
 dx1 = np.prod(nx1)
 dx2 = np.prod(nx2)
@@ -41,15 +50,30 @@ m2 = nx2.size
 x1 = np.zeros(dx1)
 x2 = np.zeros(dx2)
 
-for i in range(dx1):
-    vec_index = CombIndexToVecIndex(i, nx1)
-    if (vec_index == x1_0).all():
-        x1[i] = 1.0
+stride = 1
+idx1 = 0
+for i, el in enumerate(x1_0):
+    idx1 += el * stride
+    stride *= nx1[i]
 
-for i in range(dx2):
-    vec_index = CombIndexToVecIndex(i, nx2)
-    if (vec_index == x2_0).all():
-        x2[i] = 1.0
+stride = 1
+idx2 = 0
+for i, el in enumerate(x2_0):
+    idx2 += el * stride
+    stride *= nx2[i]
+
+x1[idx1] = 1.0
+x2[idx2] = 1.0
+
+# for i in range(dx1):
+#     vec_index = CombIndexToVecIndex(i, nx1)
+#     if (vec_index == x1_0).all():
+#         x1[i] = 1.0
+
+# for i in range(dx2):
+#     vec_index = CombIndexToVecIndex(i, nx2)
+#     if (vec_index == x2_0).all():
+#         x2[i] = 1.0
 
 fmt = '%1.16f'
 np.savetxt("input/x1_input.csv", x1, delimiter = ",", fmt = fmt)
