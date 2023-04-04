58906ea18f5febe6f58e58324193e97a4c1b7f6b
Julian Mangott <julian.mangott@uibk.ac.at>
2023-04-04 19:35:10 +0200

    Delete CSV routines

--- include/io_functions.hpp
+++ include/io_functions.hpp
@@ -16,17 +16,9 @@
 #include "grid_class.hpp"
 #include "reaction_class.hpp"
 
-
-// Read multi_array from a .csv file
-void ReadCSV(multi_array<double, 2> &output_array, std::string filename);
-
-
-// Write multi_array to a .csv file
-void WriteCSV(const multi_array<double, 2> &input_array, std::string filename);
-
-
 // Write `lr_sol` to a .netCDF file
 void WriteNC(std::string fn, const lr2<double> &lr_sol, vector<string> names, grid_info grid, double *t, double *dt);
+
 
 // Read a .netCDF file to multi_arrays `xx1` and `xx2`
 void ReadNC(std::string fn, multi_array<double, 2> &xx1, multi_array<double, 2> &xx2);
--- src/io_functions.cpp
+++ src/io_functions.cpp
@@ -4,103 +4,12 @@ using std::ifstream;
 using std::string;
 using std::stringstream;
 
-
-void ReadCSV(multi_array<double, 2> &output_array, string filename)
-{
-    Index i = 0;
-    Index j = 0;
-    string line;
-    ifstream input_file(filename);
-    stringstream ss_line;
-    string element;
-
-    while (getline(input_file, line))
-    {
-        ss_line.str(line);
-        while (getline(ss_line, element, ','))
-        {
-            output_array(i, j) = stod(element);
-            j++;
-        }
-        i++;
-        j = 0;
-        ss_line.clear();
-    }
-    input_file.close();
-}
-
-
-void WriteCSV(const multi_array<double, 2> &input_array, string filename)
-{
-    stringstream filename_type;
-    filename_type << filename << ".csv";
-    ofstream output_file(filename_type.str());
-    Index n_rows, n_cols;
-    n_rows = input_array.shape()[0];
-    n_cols = input_array.shape()[1];
-    for (Index i = 0; i < n_rows; i++)
-    {
-        for (Index j = 0; j < n_cols; j++)
-        {
-            output_file << input_array(i, j) << ',';
-        }
-        output_file << endl;
-    }
-    output_file.close();
-}
-
-
-// void WriteNetCDF(string fn, const lr2<double> &lr_sol, const mysys &reaction_system, grid_info grid, double *t, double *dt)
-// {
-//     nc_writer ncw(fn, {grid.r, grid.m1, grid.m2, grid.dx1, grid.dx2}, {"r", "m1", "m2", "dx1", "dx2"});
-//     ncw.add_var("n1", {"m1"});
-//     ncw.add_var("n2", {"m2"});
-//     ncw.add_var("X", {"r", "dx1"});
-//     ncw.add_var("S", {"r", "r"});
-//     ncw.add_var("V", {"r", "dx2"});
-
-//     ncw.start_write_mode();
-
-//     vector<double> vec_r(1), vec_m1(1), vec_m2(1), vec_dx1(1), vec_dx2(1);
-//     vec_r[0] = grid.r;
-//     vec_m1[0] = grid.m1;
-//     vec_m2[0] = grid.m2;
-//     vec_dx1[0] = grid.dx1;
-//     vec_dx2[0] = grid.dx2;
-
-//     vector<double> vec_n1(grid.m1), vec_n2(grid.m2);
-//     for (Index i = 0; i < grid.m1; i++)
-//         vec_n1[i] = grid.n1(i);
-//     for (Index i = 0; i < grid.m2; i++)
-//         vec_n2[i] = grid.n2(i);
-
-//     ncw.write("t", t);
-//     ncw.write("t", dt);
-
-//     // ncw.write("names", reaction_system.species_names.data());
-
-//     // ncw.write("r", vec_r.data());
-//     // ncw.write("m1", vec_m1.data());
-//     // ncw.write("m2", vec_m2.data());
-//     // ncw.write("dx1", vec_dx1.data());
-//     // ncw.write("dx2", vec_dx2.data());
-//     ncw.write("n1", vec_n1.data());
-//     ncw.write("n2", vec_n2.data());
-//     ncw.write("liml1", grid.liml1.data());
-//     ncw.write("liml2", grid.liml2.data());
-//     ncw.write("limr1", grid.limr1.data());
-//     ncw.write("limr2", grid.limr2.data());
-
-//     ncw.write("X", lr_sol.X.data());
-//     ncw.write("S", lr_sol.S.data());
-//     ncw.write("V", lr_sol.V.data());
-// }
-
 #define ERROR_NETCDF(e)                                                         \
     {                                                                           \
         printf("NetCDF Error %s:%i: %s\n", __FILE__, __LINE__, nc_strerror(e)); \
         exit(1);                                                                \
     }
+
 
 void WriteNC(string fn, const lr2<double> &lr_sol, vector<string> names, grid_info grid, double *t, double *dt)
 {
