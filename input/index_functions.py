d40e7ac3379bffa7b7534e2c6672671072b5b3b0
Julian Mangott <julian.mangott@uibk.ac.at>
2023-04-06 12:38:42 +0200

    Add type hints and `CombIndexToState` function

--- input/index_functions.py
+++ input/index_functions.py
@@ -2,7 +2,7 @@ from numba import njit
 import numpy as np
 
 @njit
-def CombIndexToVecIndex(comb_index, interval):
+def CombIndexToVecIndex(comb_index: int, interval: np.ndarray) -> np.ndarray:
     vec_index = np.zeros(interval.size, dtype="int64")
     for i, int_ele in enumerate(interval):
         if i == interval.size - 1:
@@ -13,10 +13,20 @@ def CombIndexToVecIndex(comb_index, interval):
     return vec_index
 
 @njit
-def VecIndexToCombIndex(vec_index, interval):
+def VecIndexToCombIndex(vec_index: np.ndarray, interval: np.ndarray) -> int:
     stride = 1
     comb_index = 0
     for i, int_ele in enumerate(interval):
         comb_index += stride * vec_index[i]
         stride *= int_ele
     return comb_index
+
+def CombIndexToState(comb_index: int, interval: np.ndarray, liml: np.ndarray, binsize: np.ndarray) -> np.ndarray:
+    dim = interval.size
+    state = np.zeros(dim)
+    for i in range(dim - 1):
+        state[i] = liml[i] + binsize[i] * (comb_index % interval[i])
+        comb_index = comb_index // interval[i]
+    if (dim > 0): 
+        state[dim - 1] = liml[dim - 1] + binsize[dim - 1] * comb_index
+    return state
