fa1c1c491398506e407ab506f4e5af2f548e54c3
Julian Mangott <julian.mangott@uibk.ac.at>
2023-04-07 11:33:42 +0200

    Remove type hints for tuple

--- input/input_helper.py
+++ input/input_helper.py
@@ -6,7 +6,7 @@ from index_functions import *
 class grid_info:
     """Class for storing grid parameters."""
 
-    def __init__(self, _r: int, _m1: int, _m2: int, _n1: tuple[int], _n2: tuple[int], _binsize1: tuple[int], _binsize2: tuple[int], _liml1: tuple[int], _liml2: tuple[int]):
+    def __init__(self, _r: int, _m1: int, _m2: int, _n1: tuple, _n2: tuple, _binsize1: tuple, _binsize2: tuple, _liml1: tuple, _liml2: tuple):
         self.r = _r
         self.m1 = _m1
         self.m2 = _m2
@@ -45,7 +45,7 @@ def WriteNC(x1: np.ndarray, x2: np.ndarray, s: np.ndarray) -> None:
     ds.close()
 
 
-def SetInputKD(x10: tuple[int], x20: tuple[int], grid: grid_info) -> None:
+def SetInputKD(x10: tuple, x20: tuple, grid: grid_info) -> None:
     """
     Creates a netCDF file containing X1, X2 and S
     for an initial distribution of Kronecker delta form.
