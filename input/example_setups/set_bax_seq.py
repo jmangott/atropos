d527c46a059180a80dc42f4f3351331dcb7f5b64
Julian Mangott <julian.mangott@uibk.ac.at>
2023-05-31 16:56:14 +0200

    Swap partitions in `set_bax_seq` script

--- input/example_setups/set_bax_seq.py
+++ input/example_setups/set_bax_seq.py
@@ -28,17 +28,17 @@ params = parser.parse_args()
 # Set in-script parameters
 params.kModel = "reactions_bax_seq"
 r = params.kR = 4
-m1 = params.kM1 = 4
-m2 = params.kM2 = 7
-n1 = params.kN1 = (91, 36, 21, 11)
-n2 = params.kN2 = (6, 6, 6, 6, 6, 21, 6)
-bin1 = params.kBinsize1 = (1, 1, 1, 1)
-bin2 = params.kBinsize2 = (1, 1, 1, 1, 1, 1, 1)
-liml1 = params.kLiml1 = (0, 0, 0, 0)
-liml2 = params.kLiml2 = (0, 0, 0, 0, 0, 90, 0)
+m1 = params.kM1 = 7
+m2 = params.kM2 = 4
+n1 = params.kN1 = (6, 6, 6, 6, 6, 21, 6)
+n2 = params.kN2 = (91, 36, 21, 11)
+bin1 = params.kBinsize1 = (1, 1, 1, 1, 1, 1, 1)
+bin2 = params.kBinsize2 = (1, 1, 1, 1)
+liml1 = params.kLiml1 = (0, 0, 0, 0, 0, 90, 0)
+liml2 = params.kLiml2 = (0, 0, 0, 0)
 
-x10 = (80, 0, 0, 0)
-x20 = (0, 0, 0, 0, 0, 100, 0)
+x10 = (0, 0, 0, 0, 0, 100, 0)
+x20 = (80, 0, 0, 0)
 
 # Change `parameters.hpp`
 params.configure()
