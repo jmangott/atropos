27b914ea9378adbd0b7608b941b5679c56634cb5
Julian Mangott <julian.mangott@uibk.ac.at>
2023-06-07 23:26:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from distutils.core import setup
 setup(name='kinetic-cme-scripts',
       version='1.0',
       description='Scripts for generating input files and plotting output files',
-      py_modules = ['scripts.index_functions']
+      py_modules = ['scripts.index_functions', 
+                    'scripts.input.parameters_helper',
+                    'scripts.input.input_helper']
       )
