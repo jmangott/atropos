386684addfd1fb05cb5683304e5808740afae349
Julian Mangott <julian.mangott@uibk.ac.at>
2023-09-26 11:15:30 +0200

    Add sanity checks and `__str__` method

--- scripts/hierarchical/grid_class.py
+++ scripts/hierarchical/grid_class.py
@@ -3,9 +3,20 @@ import numpy.typing as npt
 
 class GridParms:
     def __init__(self, _n: npt.NDArray[np.int_], _binsize: npt.NDArray[np.int_], _liml: npt.NDArray[np.float_]):
+        if (_n.size != _binsize.size or 
+            _n.size != _liml.size or
+            _binsize.size != _liml.size):
+            raise ValueError("Input arrays must be of equal length")
+        
+        if np.any(_n == 0):
+            raise ValueError("Grid size `n` must be larger than 0")
+
         self.n = _n
         self.binsize = _binsize
         self.liml = _liml
         self.dx = np.prod(self.n)
         self.h_mult = np.prod(self.binsize)
         self.d = self.n.size
+    
+    def __str__(self):
+        return ", ".join([str(ele) for ele in self.n])
