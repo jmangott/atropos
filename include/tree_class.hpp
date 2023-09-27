fd5d7e5176737b32c4c5a06a73e7098527e1ec4d
Julian Mangott <julian.mangott@uibk.ac.at>
2023-09-27 22:21:04 +0200

    Add CME-specific classes

--- include/tree_class.hpp
+++ include/tree_class.hpp
@@ -10,7 +10,7 @@
 #include "grid_class.hpp"
 
 
-// struct for storing low-rank factors and coefficients
+// General classes for the hierarchical DLR approximation
 template<class T>
 struct node
 {
@@ -23,11 +23,10 @@ struct node
 template<class T>
 struct root : node<T>
 {
-    grid_parms grid;
     T root_coeff;
     multi_array<double, 2> S;
 
-    root(grid_parms _grid, Index _r) : grid(_grid), S({_r, _r}) {};
+    root(Index _r) : S({_r, _r}) {};
 
     Index rank() const
     {
@@ -39,14 +38,13 @@ template<class T>
 struct internal_node : node<T>
 {
     node<T>* parent;
-    grid_parms grid;
     T internal_coeff;
     multi_array<double, 3> Q;
     multi_array<double, 3> G;
     multi_array<double, 2> S;
 
-    internal_node(root<T>* _parent, grid_parms _grid, Index _r) : grid(_grid), Q({_parent->rank(), _r, _r}), G({_parent->rank(), _r, _r}), S({_r, _r}) {};
-    internal_node(internal_node<T>* _parent, grid_parms _grid, Index _r) : grid(_grid), Q({_parent->rank(), _r, _r}), G({_parent->rank(), _r, _r}), S({_r, _r}) {};
+    internal_node(root<T>* _parent, Index _r) : Q({_parent->rank(), _r, _r}), G({_parent->rank(), _r, _r}), S({_r, _r}) {};
+    internal_node(internal_node<T>* _parent, Index _r) : Q({_parent->rank(), _r, _r}), G({_parent->rank(), _r, _r}), S({_r, _r}) {};
 
     Index rank() const
     {
@@ -58,12 +56,11 @@ template<class T>
 struct external_node : node<T>
 {
     node<T>* parent;
-    grid_parms grid;
     T external_coeff;
     multi_array<double, 2> X;
 
-    external_node(root<T>* _parent, grid_parms _grid) : grid(_grid), X({_parent->rank(), _grid.dx}) {};
-    external_node(internal_node<T>* _parent, grid_parms _grid) : grid(_grid), X({_parent->rank(), _grid.dx}) {};
+    external_node(root<T>* _parent, Index _dx) : X({_parent->rank(), _dx}) {};
+    external_node(internal_node<T>* _parent, Index _dx) : X({_parent->rank(), _dx}) {};
 
     Index problem_size() const
     {
@@ -72,9 +69,39 @@ struct external_node : node<T>
 };
 
 template<class T>
-struct tree
+struct lr_tree
 {
     root<T>* root_node;
+
+    // lr_tree(root<T>* _root_node) : root_node(_root_node) {};
+};
+
+
+// Dervied, CME-specific classes
+template<class T>
+struct cme_root : root<T>
+{
+    grid_parms grid;
+
+    cme_root(grid_parms _grid, Index _r) : root<T>(_r), grid(_grid) {};
+};
+
+template<class T>
+struct cme_internal_node : internal_node<T>
+{
+    grid_parms grid;
+
+    cme_internal_node(root<T> *_parent, grid_parms _grid, Index _r) : internal_node<T>(_parent, _r), grid(_grid){};
+    cme_internal_node(internal_node<T> *_parent, grid_parms _grid, Index _r) : internal_node<T>(_parent, _r), grid(_grid) {};
+};
+
+template<class T>
+struct cme_external_node : external_node<T>
+{
+    grid_parms grid;
+
+    cme_external_node(root<T> *_parent, grid_parms _grid) : external_node<T>(_parent, _grid.dx()), grid(_grid) {};
+    cme_external_node(internal_node<T> *_parent, grid_parms _grid) : external_node<T>(_parent, _grid.dx()), grid(_grid) {};
 };
 
 #endif
