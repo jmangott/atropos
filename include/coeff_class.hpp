5773bca0899f39d13a41d9347d7aa3d3478c8bcb
Julian Mangott <julian.mangott@uibk.ac.at>
2023-11-28 13:21:38 +0100

    Extend coefficient classes

--- include/coeff_class.hpp
+++ include/coeff_class.hpp
@@ -10,23 +10,36 @@
 #include "grid_class.hpp"
 
 
-struct coeff {};
+struct coeff
+{
+    std::vector<std::vector<double>> propensity;
 
-// struct cme_root_coeff : coeff
-// {
-//     std::vector<multi_array<double, 2>> propensity;
-// };
+    explicit coeff(const Index _n_reactions) : propensity(_n_reactions) {}
+};
 
 struct cme_internal_coeff : coeff
 {
-    multi_array<double, 6> g_coeff, h_coeff;
+    multi_array<double, 4> E, F;
+    multi_array<double, 6> G, H;
+
+    cme_internal_coeff(const Index _n_reactions, const Index r_in, const Index r_out) 
+    : coeff(_n_reactions)
+    , E({r_out, r_out, r_out, r_out})
+    , F({r_out, r_out, r_out, r_out})
+    , G({r_in, r_out, r_out, r_in, r_out, r_out})
+    , H({r_in, r_out, r_out, r_in, r_out, r_out})
+    {}
 };
 
 struct cme_external_coeff : coeff
 {
-    std::vector<multi_array<double, 3>> c_coeff, d_coeff;
-    multi_array<double, 4> e_coeff, f_coeff;
+    std::vector<multi_array<double, 3>> C, D;
+    
+    explicit cme_external_coeff(const Index n_reactions)
+    : coeff(n_reactions)
+    , C(n_reactions)
+    , D(n_reactions)
+    {}
 };
-
 
 #endif
