60e36793a09aad656941bc825a34ffbcd5970d65
Julian Mangott <julian.mangott@uibk.ac.at>
2024-01-23 16:27:37 +0100

    Fix several bugs

--- scripts/hierarchical/tree_class.py
+++ scripts/hierarchical/tree_class.py
@@ -1,6 +1,7 @@
 """
 Contains the `Tree` class for storing the low-rank approximation od the initial probability distribution as a binary tree according to a prescribed partition and writing it to a netCDF file.
 """
+import copy
 from datatree import DataTree
 import numpy as np
 import numpy.typing as npt
@@ -9,17 +10,17 @@ import regex
 from typing import Union
 import xarray as xr
 
-from grid_class import GridParms
-from id_class import Id
-from reaction_class import *
-from scripts.index_functions import *
+from scripts.hierarchical.grid_class import GridParms
+from scripts.hierarchical.id_class import Id
+from scripts.hierarchical.reaction_class import ReactionSystem
+from scripts.index_functions import incrVecIndex
 
 class Node:
-    def __init__(self, _id: Id, _grid: GridParms, _species_limits: list):
+    def __init__(self, _id: Id, _grid: GridParms, _species: list):
         self.child = [None] * 2
         self.id = _id
         self.grid = _grid
-        self.species_limits = _species_limits
+        self.species = _species
         self.propensity = []
 
 class InternalNode(Node):
@@ -35,8 +36,12 @@ class ExternalNode(Node):
         super().__init__(_id, _grid, _species_limits)
         self.parent = _parent
         self.r_in = self.parent.r_out
-        self.X = np.zeros((self.grid.dx, self.r_in))
+        self.X = np.zeros((self.grid.dx(), self.r_in))
 
+
+"""
+The `tree` class stores the initial condition for the hierarchical DLR approximation of the chemical master equation. Note that the input for the grid parameters (`_grid`) must follow the same ordering convention for the species as the reaction system (`_reaction_system`), but the partition string for dividing the reaction networks allows also permutation.
+"""
 class Tree:
     def __init__(self,
                  _reaction_system: ReactionSystem,
@@ -46,26 +51,30 @@ class Tree:
 
         # Test whether `_partition_str` is a valid input string
         p = self.__removeBrackets(_partition_str)
+        
+        species = np.copy(p)  # Create a deep copy of `p` before sorting it
+        p.sort()
+
         p_diff = np.diff(p)
         if not regex.fullmatch(r"\((?:\d+(?:\s\d)*|(?R))+\)\((?:\d+(?:\s\d)*|(?R))+\)", _partition_str):
-            raise ValueError("Not a valid input string")
+            raise Exception("Not a valid `_partition_str`")
         if p[0] != 0:
-            raise ValueError("The first numerical value has to be 0")
+            raise Exception("The smallest value of `_partition_str` has to be 0")
         elif np.any(p_diff != 1):
-            raise ValueError("Numerical values have to be in ascending order")
+            raise Exception("Not all species covered by `_partition_str`")
 
         # Test whether parentheses are closed
         sigma = 0
         for ele in _partition_str:
             sigma += self.__parsingHelper(ele)
             if sigma < 0:
-                raise ValueError("Left bracket is missing in `_partition_str`")
+                raise Exception("Left bracket is missing in `_partition_str`")
         if sigma != 0:
-            raise ValueError("Right bracket is missing is `_partition_str`")
+            raise Exception("Right bracket is missing is `_partition_str`")
 
         # Test whether the dimension of `_partition_str` and `_grid.d` match
-        if p.size != _grid.d:
-            raise ValueError(
+        if p.size != _grid.d():
+            raise Exception(
                 "Dimensions of `_partition_str` and `_grid.d` do not match")
 
         # Calculate the number of internal nodes
@@ -73,18 +82,19 @@ class Tree:
 
         # Test whether the dimension of `_r` is equal to n_internal_nodes
         if _r_out.size != self.n_internal_nodes:
-            raise ValueError(
+            raise Exception(
                 "`_r_out.size` must be equal to the number of internal nodes")
         
         # Calculate the number of external nodes
-        self.n_external_nodes = len(regex.findall(r"\(\d+(?:\s\d)*\)", _partition_str))
+        self.n_external_nodes = len(regex.findall(r"\(\d+(?:\s\d)*\)", _partition_str)) + 1
 
         self.reaction_system = _reaction_system
         self.partition_str = _partition_str
-        self.grid = _grid
+        self.grid = copy.deepcopy(_grid)
         self.r_out = _r_out
         self.grid.initialize(self.reaction_system)
-        self.root = InternalNode(None, Id(""), self.grid, [0, self.grid.d - 1], 1, self.r_out[0])
+        self.grid.permute(species)
+        self.root = InternalNode(None, Id(""), self.grid, species, 1, self.r_out[0])
 
     @staticmethod
     def __parsingHelper(input_str):
@@ -127,28 +137,26 @@ class Tree:
 
         partition_str0 = partition_str[1:i]
         partition_str1 = partition_str[i+2:-1]
-
-        p0 = self.__removeBrackets(partition_str0)
+        species0 = self.__removeBrackets(partition_str0)
+        species1 = self.__removeBrackets(partition_str1)
+        p0_size = species0.size
 
         grid0 = GridParms(
-            node.grid.n[:p0.size], node.grid.binsize[:p0.size], node.grid.liml[:p0.size], node.grid.dep[ :p0.size, :], node.grid.nu[:p0.size, :])
+            node.grid.n[:p0_size], node.grid.binsize[:p0_size], node.grid.liml[:p0_size], node.grid.dep[:p0_size, :], node.grid.nu[:p0_size, :])
         grid1 = GridParms(
-            node.grid.n[p0.size:], node.grid.binsize[p0.size:], node.grid.liml[p0.size:], node.grid.dep[p0.size:, :], node.grid.nu[p0.size:, :])
+            node.grid.n[p0_size:], node.grid.binsize[p0_size:], node.grid.liml[p0_size:], node.grid.dep[p0_size:, :], node.grid.nu[p0_size:, :])
         
-        species_limits0 = [node.species_limits[0], node.species_limits[0] + (grid0.d - 1)]
-        species_limits1 = [node.species_limits[1] - (grid1.d - 1), node.species_limits[1]]
-
         if (partition_str0[0] == "("):
-            node.child[0] = InternalNode(node, node.id + 0, grid0, species_limits0, node.r_out, next(r_out_iter))
+            node.child[0] = InternalNode(node, node.id + 0, grid0, species0, node.r_out, next(r_out_iter))
             self.__buildTree(node.child[0], partition_str0, r_out_iter)
         else:
-            node.child[0] = ExternalNode(node, node.id + 0, grid0, species_limits0)
+            node.child[0] = ExternalNode(node, node.id + 0, grid0, species0)
 
         if (partition_str1[0] == "("):
-            node.child[1] = InternalNode(node, node.id + 1, grid1, species_limits1, node.r_out, next(r_out_iter))
+            node.child[1] = InternalNode(node, node.id + 1, grid1, species1, node.r_out, next(r_out_iter))
             self.__buildTree(node.child[1], partition_str1, r_out_iter)
         else:
-            node.child[1] = ExternalNode(node, node.id + 1, grid1, species_limits1)
+            node.child[1] = ExternalNode(node, node.id + 1, grid1, species1)
 
         self.__calculatePropensity(node.child[0], self.reaction_system)
         self.__calculatePropensity(node.child[1], self.reaction_system)
@@ -158,12 +166,11 @@ class Tree:
     def __calculatePropensity(node: Node, reaction_system: ReactionSystem):
         node.propensity = [None] * reaction_system.size()
         for mu, reaction in enumerate(reaction_system.reactions):
-            n_dep = np.array([node.grid.n[i] for i in range(node.grid.d) if node.grid.dep[i, mu]], dtype="int")
+            n_dep = np.array([n for i, n in enumerate(node.grid.n) if node.grid.dep[i, mu]], dtype="int")
             dx_dep = np.prod(n_dep)
             node.propensity[mu] = np.ones(dx_dep)
             vec_index = np.zeros(n_dep.size)
-            reactants = [reactant for reactant in reaction.propensity.keys() if node.species_limits[0] <= reactant <= node.species_limits[1]]
-
+            reactants = [reactant for reactant in node.species if reactant in reaction.propensity.keys()]
             for i in range(dx_dep):
                 for j, reactant in enumerate(reactants):
                     node.propensity[mu][i] *= reaction.propensity[reactant](vec_index[j])
@@ -175,7 +182,7 @@ class Tree:
         self.__buildTree(self.root, self.partition_str, r_out_iter)
 
     def __printTree(self, node: Node, os: str) -> str:
-        os = " ".join([os, str(type(node)), "id:", str(node.id), "n:", str(node.grid), "species_limits:", str(node.species_limits)])
+        os = " ".join([os, str(type(node)), "id:", str(node.id), "n:", str(node.grid), "species:", str(node.species)])
         if isinstance(node, ExternalNode):
             os = " ".join([os, "X.shape:", str(node.X.shape), "\n"])
         else:
