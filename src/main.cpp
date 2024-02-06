160af8704b1f761463dc11f8023ec71832a37497
Julian Mangott <julian.mangott@uibk.ac.at>
2024-02-06 20:52:56 +0100

    Initialize `dm`

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
     get_time::start("main");
 
     double t = 0.0;
-    double dm;
+    double dm = 0.0;
     double dm_max = 0.0;
     blas_ops blas;
     implicit_euler method;
