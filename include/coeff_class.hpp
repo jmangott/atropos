c25f02fe4ca3537c44687ffc804830583e845ed2
Julian Mangott <julian.mangott@uibk.ac.at>
2024-02-09 12:41:05 +0100

    Change container for A and B coefficients

--- include/coeff_class.hpp
+++ include/coeff_class.hpp
@@ -11,36 +11,39 @@
 
 struct cme_coeff
 {
-    multi_array<double, 3> A, B;
+    std::vector<multi_array<double, 2>> A, B;
     multi_array<double, 4> E, F;
 
     cme_coeff(const Index _n_reactions, const Index _r_in)
-    : A({_n_reactions, _r_in, _r_in})
-    , B({_n_reactions, _r_in, _r_in})
+    : A(_n_reactions)
+    , B(_n_reactions)
     , E({_r_in, _r_in, _r_in, _r_in})
     , F({_r_in, _r_in, _r_in, _r_in})
-    {}
+    {
+        for (Index mu = 0; mu < _n_reactions; ++mu)
+        {
+            A[mu].resize({_r_in, _r_in});
+            B[mu].resize({_r_in, _r_in});
+        }
+    }
 };
 
 struct cme_internal_coeff
 {
-    multi_array<double, 6> G, H;
+    multi_array<double, 4> G, H;
 
     cme_internal_coeff(const Index _r_in, const std::array<Index, 2> _r_out) 
-    : G({_r_in, _r_out[0], _r_out[1], _r_in, _r_out[0], _r_out[1]})
-    , H({_r_in, _r_out[0], _r_out[1], _r_in, _r_out[0], _r_out[1]})
+    : G({_r_in, prod(_r_out), _r_in, prod(_r_out)})
+    , H({_r_in, prod(_r_out), _r_in, prod(_r_out)})
     {}
 };
 
 struct cme_external_coeff
 {
     std::vector<std::vector<double>> propensity;
-    std::vector<multi_array<double, 3>> C, D;
 
     cme_external_coeff(const Index _n_reactions)
     : propensity(_n_reactions)
-    , C(_n_reactions)
-    , D(_n_reactions)
     {}
 };
 
