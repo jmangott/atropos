6e129edca65a65cf9f1fd363f12b631c040e7ff5
Julian Mangott <julian.mangott@uibk.ac.at>
2024-08-29 17:21:14 +0200

    Change type in `Reaction` constructor

--- scripts/reaction_class.py
+++ scripts/reaction_class.py
@@ -4,14 +4,14 @@ Contains the `Reaction` class for a single reaction of the reaction network and 
 import numpy as np
 
 class Reaction:
-    def __init__(self, _propensity: dict, _nu: list):
+    def __init__(self, _propensity: dict, _nu: np.ndarray[int]):
         self.propensity = dict(sorted(_propensity.items()))
         self.nu = _nu
 
 class ReactionSystem:
     def __init__(self, _reactions: list[Reaction], _species_names: list[str]):
         for reaction in _reactions:
-            if (len(reaction.nu) != len(_species_names)):
+            if (reaction.nu.size != len(_species_names)):
                 raise ValueError("Dimensions of `nu` and `_species_names` do not match")
 
         self.reactions = _reactions
