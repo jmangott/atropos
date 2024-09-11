45f2ccaf5e9bf9a78458ebaaf7bfdd9a1258aacf
Julian Mangott <julian.mangott@uibk.ac.at>
2024-09-11 09:44:44 +0200

    Add commands for reference solutions

--- scripts/shell/run_boolean_apoptosis.sh
+++ scripts/shell/run_boolean_apoptosis.sh
@@ -24,5 +24,10 @@ python3 scripts/input_generation/set_boolean_apoptosis.py -pw -r 5
 python3 scripts/input_generation/set_boolean_apoptosis.py -pw -r 10
 ./bin/hierarchical-cme -o apoptosis_pw_r10_e_tau1e-2 -s 100 -t 0.01 -f 20.0 -m e
 
-python3 scripts/input_generation/set_boolean_apoptosis.py -pw -r 20
-./bin/hierarchical-cme -o apoptosis_pw_r20_e_tau1e-2 -s 100 -t 0.01 -f 20.0 -m e
+
+# Reference solution
+python3 scripts/input_generation/set_boolean_apoptosis.py -pb -r 30
+./bin/hierarchical-cme -o apoptosis_pb_r30_e_tau1e-2 -s 100 -t 0.01 -f 20.0 -m e
+
+python3 scripts/input_generation/set_boolean_apoptosis.py -pb -r 50
+./bin/hierarchical-cme -o apoptosis_pb_r50_r_tau1e-2 -s 100 -t 0.01 -f 20.0 -m r
--- scripts/shell/run_boolean_pancreatic_cancer.sh
+++ scripts/shell/run_boolean_pancreatic_cancer.sh
@@ -26,3 +26,11 @@ python3 scripts/input_generation/set_boolean_pancreatic_cancer.py -pw -r 10
 
 python3 scripts/input_generation/set_boolean_pancreatic_cancer.py -pw -r 20
 ./bin/hierarchical-cme -o pancreatic_pw_r20_e_tau1e-2 -s 100 -t 0.01 -f 20.0 -m e
+
+
+# Reference solution
+python3 scripts/input_generation/set_boolean_pancreatic_cancer.py -pb -r 30
+./bin/hierarchical-cme -o pancreatic_pb_r30_e_tau1e-2 -s 100 -t 0.01 -f 20.0 -m e
+
+python3 scripts/input_generation/set_boolean_pancreatic_cancer.py -pb -r 50
+./bin/hierarchical-cme -o pancreatic_pb_r50_r_tau5e-3 -s 200 -t 0.005 -f 20.0 -m r
