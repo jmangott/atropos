b4fe4730403d6108db9a235fe0d2f4f3806e7f3e
Julian Mangott <julian.mangott@uibk.ac.at>
2024-11-14 12:52:22 +0100

    Add graph script for pancreatic matrix

---
+++ scripts/notebooks/output_boolean_pancreatic_cancer_graph.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors
+import numpy as np
+
+import scripts.boolean_helper
+from scripts.grid_class import GridParms
+from scripts.tree_class import Tree, plotReactionGraph
+
+reaction_system = scripts.boolean_helper.convertRulesToReactions("scripts/models/boolean_rulefiles/pancreatic_cancer.hpp")
+
+plt.style.use("./scripts/notebooks/custom_style_boolean.mplstyle")
+
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+d = 34
+n = 2 * np.ones(d, dtype=int)
+binsize = np.ones(d, dtype=int)
+liml = np.zeros(d)
+grid = GridParms(n, binsize, liml)
+
+tree = Tree(
+    "(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)(17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33)",
+    grid,
+)
+r_out = np.ones(tree.n_internal_nodes, dtype="int") * 5
+tree.initialize(reaction_system, r_out)
+
+plotReactionGraph(
+    tree.G,
+    "plots/pancreatic_graph_literature.pdf",
+    mode="partitions",
+    color_hex=matplotlib.colors.to_hex((0.55, 0.55, 0.55)),
+    prog="neato",
+    seed=5,
+)
