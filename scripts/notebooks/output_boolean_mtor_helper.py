4008dde849f7793803765af7b1e2407265f429e1
Julian Mangott <julian.mangott@uibk.ac.at>
2024-11-14 11:04:15 +0100

    Add scripts and notebooks for the pancreatic model

---
+++ scripts/notebooks/output_boolean_mtor_helper.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from scripts.tree_class import Tree
+from scripts.notebooks.output_helper import *
+
+def saveMoments(filename):
+    time_series = TimeSeries(filename)
+    time = time_series.time
+    moments = time_series.calculateMoments()
+    np.savez("{}.npz".format(filename), time=time, moments=moments)
+
+partitions = ["b", "r"] #, "w", "l"]
+ranks = [5, 10, 20]
+
+for p in partitions:
+    for r in ranks:
+        print("partition:", p, "rank:", r)
+        saveMoments("output/pancreatic_matrix_p{}_r{}_e_tau1e-2".format(p, r))
