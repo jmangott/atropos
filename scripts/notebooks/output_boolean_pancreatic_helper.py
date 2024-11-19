39ac9cdc16774c60db0309a3e42f37b4bab1a6f5
Julian Mangott <julian.mangott@uibk.ac.at>
2024-11-19 15:18:56 +0100

    Update helper script for pancreatic cancer model

--- scripts/notebooks/output_boolean_pancreatic_helper.py
+++ scripts/notebooks/output_boolean_pancreatic_helper.py
@@ -9,10 +9,15 @@ def saveMoments(filename):
     moments = time_series.calculateMoments()
     np.savez("{}.npz".format(filename), time=time, moments=moments)
 
-partitions = ["b", "r"] #, "w", "l"]
+partitions = ["b", "r", "w", "l"]
 ranks = [5, 10, 20]
-
 for p in partitions:
     for r in ranks:
         print("partition:", p, "rank:", r)
         saveMoments("output/pancreatic_matrix_p{}_r{}_e_tau1e-2".format(p, r))
+
+# Reference solutions
+ranks = [50, 60]
+for r in ranks:
+    print("reference solution, rank:", r)
+    saveMoments("output/pancreatic_matrix_pl_r{}_e_tau1e-2".format(r))
