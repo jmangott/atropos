Fix overflow bug in `boolean_helper.py`

scripts/boolean_helper.py
@@ -19,8 +19,6 @@ def fun_x1(x):
 def convertRulesToReactions(filename: str):
     """
     Converts a rule file of the Boolean CME integrator of https://bitbucket.org/mprugger/low_rank_cme to a instance of the `ReactionSystem` class.
-    NOTE: This method is only capable of converting systems with a maximum of 64 species.
-    Apparently this only works correctly on macOS, for Fedora 36 (and Linux in general?) there occurs an overflow in lines 85–86.
     """
     with open(filename) as f:
         line0 = f.readline()
@@ -84,7 +82,8 @@ def convertRulesToReactions(filename: str):
                 x[k_dep] = x_dep[k]
             curr_rule = handle["rule_{}".format(i)]
             curr_rule.argtypes = [ctypes.c_ulonglong] # avoid overflow for large systems
-            x_i_prime = curr_rule(bitarray.util.ba2int(x))
+            curr_rule.restype = ctypes.c_bool
+            x_i_prime = int(curr_rule(bitarray.util.ba2int(x)))
             if x[i] != x_i_prime: # create a reaction only when output is different from input
                 nu = np.zeros(d)
                 nu[i] = 1 if x[i] == 0 else -1

scripts/input_generation/set_boolean_apoptosis.py
@@ -9,9 +9,7 @@ from scripts.initial_condition_class import InitialCondition
 from scripts.tree_class import Tree
 from scripts.index_functions import incrVecIndex
 
-# reaction_system = scripts.boolean_helper.convertRulesToReactions("scripts/models/boolean_rulefiles/apoptosis.hpp")
-with np.load("scripts/models/boolean_rulefiles/apoptosis_macOS.npz", allow_pickle=True) as f:
-    reaction_system = f["reaction_system"][()]
+reaction_system = scripts.boolean_helper.convertRulesToReactions("scripts/models/boolean_rulefiles/apoptosis.hpp")
 
 # 24-10-03
 p_best = "((0 2 3 4 5 6 7 8 9 12 20)(21 25 26 27 28 29 30 31 39 40))((1 10 11 14 15 22 23 32 33 38)(13 16 17 18 19 24 34 35 36 37))"

scripts/input_generation/set_boolean_pancreatic_cancer.py
@@ -9,9 +9,7 @@ from scripts.initial_condition_class import InitialCondition
 from scripts.tree_class import Tree
 from scripts.index_functions import incrVecIndex
 
-# reaction_system = scripts.boolean_helper.convertRulesToReactions("scripts/models/boolean_rulefiles/pancreatic_cancer.hpp")
-with np.load("scripts/models/boolean_rulefiles/apoptosis_macOS.npz", allow_pickle=True) as f:
-    reaction_system = f["reaction_system"][()]
+reaction_system = scripts.boolean_helper.convertRulesToReactions("scripts/models/boolean_rulefiles/pancreatic_cancer.hpp")
 
 # 24-10-03
 p_best = "((0 1 2 3 4 5 7 8 9)(6 10 11 12 17 21 23 26))((13 14 19 20 25 27 29 30 32)(15 16 18 22 24 28 31 33))"