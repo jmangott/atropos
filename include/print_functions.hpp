7ff20d5ddf33a9b6e0e1f962b1f8683db0be1679
Julian Mangott <julian.mangott@uibk.ac.at>
2025-09-10 17:33:48 +0200

    Fix deprecation issues

--- include/print_functions.hpp
+++ include/print_functions.hpp
@@ -24,8 +24,48 @@ auto ChronoBurst(std::chrono::duration<Rep, std::ratio<num, denom>> d)
     return std::make_tuple(hrs, mins, secs, ms);
 }
 
-// Print progress bar
-void PrintProgressBar(const Index ts, const Index kNsteps, const std::chrono::system_clock::time_point t_start, const double norm);
+// TODO: output width is not always the same
+// TODO: calculate `time_left` with a moving mean (of the last n steps)
+template <typename T>
+void PrintProgressBar(const Index ts, const Index kNsteps, const T t_start, const double norm)
+{
+    int bar_width = 30;
+    double progress = (ts + 1.0) / kNsteps;
+    int pos = bar_width * progress;
+    string time_unit;
+    string progress_bar(bar_width, ' ');
+
+    auto t_stop(std::chrono::high_resolution_clock::now());
+    auto duration(std::chrono::duration_cast<std::chrono::seconds>(t_stop - t_start));
+    // time_per_step = duration.count() / (ts + 1.0);
+    auto time_per_step = duration / (ts + 1.0);
+    auto time_left = time_per_step * (kNsteps - 1.0 - ts);
+    double time_per_step_count;
+
+    if (time_per_step.count() < 0.01)
+    {
+        time_unit = "ms";
+        time_per_step_count = time_per_step.count() * 1000.0;
+    }
+    else if (time_per_step.count() < 60.0)
+    {
+        time_unit = "s";
+        time_per_step_count = time_per_step.count();
+    }
+    else
+    {
+        time_unit = "min";
+        time_per_step_count = time_per_step.count() / 60.0;
+    }
+
+    const auto [hrs, mins, secs, ms] = ChronoBurst(time_left);
+
+    std::fill(std::begin(progress_bar), std::begin(progress_bar) + pos, '#');
+    std::fill(std::begin(progress_bar) + pos, std::end(progress_bar), '-');
+
+    printf("[%*s], step: %ti/%ti, time per step: %.2f%*s, time left: %2.2lli:%2.2lli:%2.2lli, progress: %4.2f%%, |norm(P)-1|: %3.2e\r", bar_width, progress_bar.c_str(), ts + 1, kNsteps, time_per_step_count, (int)time_unit.size(), time_unit.c_str(), (long long int)hrs.count(), (long long int)mins.count(), (long long int)secs.count(), progress * 100, std::abs(norm - 1.0));
+    fflush(stdout);
+}
 
 struct diagnostics
 {
