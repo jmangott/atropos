0b5b50b1b20096cf5015172e509e8abe2f95e6bf
Julian Mangott <julian.mangott@uibk.ac.at>
2025-12-16 09:18:20 +0100

    Allow differing child ranks

--- scripts/initial_condition_class.py
+++ scripts/initial_condition_class.py
@@ -1,10 +1,12 @@
 """Contains the `InitialCondition` class for setting up initial conditions."""
+
 import numpy as np
 import numpy.typing as npt
 
 from scripts.tree_class import Tree, Node, ExternalNode, InternalNode
 
 # TODO: Q should have shape (n_basisfunctions, child(n_basisfunctions), child(n_basisfunctions))
+
 
 class InitialCondition:
     """
@@ -17,20 +19,28 @@ class InitialCondition:
     These arrays can be conveniently set to obey the initial conditions. For setting up the low-rank factors, the external nodes are also accessible via the array `external_nodes`.
     """
 
-    def __setNodeData(self, node: Node, nb: int):
-        if (nb > node.rankIn()):
+    def __setNodeData(self, node: Node, nb):
+        if nb > node.rankIn():
             raise Exception(
-                            "Number of basisfunctions must be smaller or equal to the incoming rank")
+                "Number of basisfunctions must be smaller or equal to the incoming rank"
+            )
 
         if isinstance(node, ExternalNode):
             node.X.resize((node.grid.dx(), nb), refcheck=False)
 
         elif isinstance(node, InternalNode):
-            node.Q.resize((node.rankOut(), node.rankOut(), nb), refcheck=False)
+            node.Q.resize((node.rankOut()[0], node.rankOut()[1], nb), refcheck=False)
 
-            next_nb = next(self.n_basisfunctions_iter)
-            self.__setNodeData(node.child[0], next_nb)
-            self.__setNodeData(node.child[1], next_nb)
+            if isinstance(self.n_basisfunctions, dict):
+                current_id = node.id
+                nb0 = self.n_basisfunctions[current_id + 0]
+                nb1 = self.n_basisfunctions[current_id + 1]
+                self.__setNodeData(node.child[0], nb0)
+                self.__setNodeData(node.child[1], nb1)
+            elif isinstance(self.n_basisfunctions, np.ndarray):
+                next_nb = next(self.n_basisfunctions_iter)
+                self.__setNodeData(node.child[0], next_nb)
+                self.__setNodeData(node.child[1], next_nb)
 
     def __getNodeData(self, node: Node):
         if isinstance(node, ExternalNode):
@@ -42,16 +52,33 @@ class InitialCondition:
             self.__getNodeData(node.child[1])
 
     def __init__(self, _tree: Tree, _n_basisfunctions: npt.NDArray[np.int_]):
-        if (_n_basisfunctions.size != _tree.n_internal_nodes):
-            raise Exception(
-                "`_n_basisfunctions.size` must be equal to the number of internal nodes")
+        if isinstance(_n_basisfunctions, np.ndarray):
+            if _n_basisfunctions.size != _tree.n_internal_nodes:
+                raise Exception(
+                    "`_n_basisfunctions.size` must be equal to the number of internal nodes"
+                )
 
-        self.n_basisfunctions_iter = iter(_n_basisfunctions)
+        if isinstance(_n_basisfunctions, dict):
+            self.n_basisfunctions = _n_basisfunctions
+        elif isinstance(_n_basisfunctions, np.ndarray):
+            self.n_basisfunctions = _n_basisfunctions
+            self.n_basisfunctions_iter = iter(_n_basisfunctions)
+        else:
+            raise Exception(
+                "`_n_basisfunctions` must be either a `dict` or a `np.ndarray`"
+            )
         self.external_nodes = []
         self.Q = []
         self.X = []
 
-        nb = next(self.n_basisfunctions_iter)
-        self.__setNodeData(_tree.root.child[0], nb)
-        self.__setNodeData(_tree.root.child[1], nb)
+        if isinstance(self.n_basisfunctions, dict):
+            current_id = _tree.root.id
+            nb0 = self.n_basisfunctions[current_id + 0]
+            nb1 = self.n_basisfunctions[current_id + 1]
+            self.__setNodeData(_tree.root.child[0], nb0)
+            self.__setNodeData(_tree.root.child[1], nb1)
+        elif isinstance(self.n_basisfunctions, np.ndarray):
+            nb = next(self.n_basisfunctions_iter)
+            self.__setNodeData(_tree.root.child[0], nb)
+            self.__setNodeData(_tree.root.child[1], nb)
         self.__getNodeData(_tree.root)
--- scripts/input_generation/set_lambda_phage.py
+++ scripts/input_generation/set_lambda_phage.py
@@ -1,4 +1,5 @@
 """Script for setting the initial conditions for the lambda phage model."""
+
 import argparse
 import numpy as np
 import sys
@@ -11,42 +12,51 @@ from scripts.index_functions import incrVecIndex, vecIndexToState, tensorUnfold
 
 import scripts.models.lambda_phage as model
 
-partition = ['(0 1)((2 3)(4))',
-             '((0 1)(2 3))(4)',
-             '((0 1)(2))(3 4)',
-             '(0 1)(2 3 4)']
+partition = ["(0 1)((2 3)(4))", "((0 1)(2 3))(4)", "((0 1)(2))(3 4)", "(0 1)(2 3 4)"]
 
 parser = argparse.ArgumentParser(
-                    prog='set_lambda_phage',
-                    usage='python3 scripts/input_generation/set_lambda_phage.py --partition "'+partition[0]+'" --rank 5 5',
-                    description='This script sets the initial conditions for the lambda phage model.')
+    prog="set_lambda_phage",
+    usage='python3 scripts/input_generation/set_lambda_phage.py --partition "'
+    + partition[0]
+    + '" --rank 5 5',
+    description="This script sets the initial conditions for the lambda phage model.",
+)
 
 for i, p in enumerate(partition):
-    parser.add_argument('-p'+str(i), 
-                        '--partition'+str(i), 
-                        action='store_const', 
-                        const=p,
-                        required=False, 
-                        help='Set the partition string to '+p,
-                        dest='partition', 
-                        )
-parser.add_argument('-r', 
-                    '--rank', 
-                    nargs='+', 
-                    type=int, 
-                    required=True, 
-                    help="Specify the ranks of the internal node",
-                    )
+    parser.add_argument(
+        "-p" + str(i),
+        "--partition" + str(i),
+        action="store_const",
+        const=p,
+        required=False,
+        help="Set the partition string to " + p,
+        dest="partition",
+    )
+parser.add_argument(
+    "-r",
+    "--rank",
+    nargs="+",
+    type=int,
+    required=True,
+    help="Specify the ranks of the internal node",
+)
 args = parser.parse_args()
 
-if args.partition == None:
+if args.partition is None:
     print("usage:", parser.usage)
-    print(parser.prog+":", "error: the following arguments are required: -p[n]/--partition[n], n=0,...,"+str(len(partition)-1))
+    print(
+        parser.prog + ":",
+        "error: the following arguments are required: -p[n]/--partition[n], n=0,...,"
+        + str(len(partition) - 1),
+    )
     sys.exit(1)
 
 partition_str = args.partition
 r_out = np.array(args.rank)
 n_basisfunctions = np.ones(r_out.size, dtype="int")
+
+# r_out = {Id("0"): args.rank[0], Id("1"): args.rank[1], Id("10"): args.rank[2], Id("11"): args.rank[3]}
+# n_basisfunctions = np.ones(len(args.rank), dtype="int")
 
 # Grid parameters
 n = np.array([16, 41, 11, 11, 11])
@@ -59,33 +69,44 @@ grid = GridParms(n, binsize, liml)
 tree = Tree(partition_str, grid)
 tree.initialize(model.reaction_system, r_out)
 
+
 # Initial distribution
 def multinomial(x):
     abs_x = np.sum(x)
-    if (abs_x <= 3):
-        p0 = factorial(3) * (0.05 ** abs_x) * \
-            ((1.0 - 5 * 0.05) ** (3 - abs_x)) / (np.prod(factorial(x)) * factorial(3 - abs_x))
+    if abs_x <= 3:
+        p0 = (
+            factorial(3)
+            * (0.05**abs_x)
+            * ((1.0 - 5 * 0.05) ** (3 - abs_x))
+            / (np.prod(factorial(x)) * factorial(3 - abs_x))
+        )
     else:
         p0 = 0.0
     return p0
 
+
 # Helper function for factorizing a low-rank factor
 def factorizeLRFactor(x, node):
-    x_tensor = np.zeros((node.child[0].grid.dx(), node.child[1].grid.dx(), node.rankIn()))
+    x_tensor = np.zeros(
+        (node.child[0].grid.dx(), node.child[1].grid.dx(), node.rankIn())
+    )
     for i in range(node.rankIn()):
-        x_tensor[:, :, i] = x[:, i].reshape((node.child[0].grid.dx(), node.child[1].grid.dx()), order="F")
+        x_tensor[:, :, i] = x[:, i].reshape(
+            (node.child[0].grid.dx(), node.child[1].grid.dx()), order="F"
+        )
 
     x_mat = tensorUnfold(x_tensor, 0)
     u, _, _ = np.linalg.svd(x_mat, full_matrices=False)
-    x0 = u[:, :node.child[0].rankIn()]
+    x0 = u[:, : node.child[0].rankIn()]
 
     x_mat = tensorUnfold(x_tensor, 1)
     u, _, _ = np.linalg.svd(x_mat, full_matrices=False)
-    x1 = u[:, :node.child[1].rankIn()]
+    x1 = u[:, : node.child[1].rankIn()]
 
-    q = np.einsum('ik,jl,ijm', x0, x1, x_tensor)
+    q = np.einsum("ik,jl,ijm", x0, x1, x_tensor)
 
     return q, x0, x1
+
 
 p0 = np.zeros(grid.dx())
 vec_index = np.zeros(grid.d())
@@ -94,15 +115,17 @@ for i in range(grid.dx()):
     p0[i] = multinomial(state + (grid.binsize - 1.0) * 0.5)
     incrVecIndex(vec_index, grid.n, grid.d())
 
-p0_mat = p0.reshape((tree.root.child[0].grid.dx(), tree.root.child[1].grid.dx()), order="F")
+p0_mat = p0.reshape(
+    (tree.root.child[0].grid.dx(), tree.root.child[1].grid.dx()), order="F"
+)
 
 # SVD of p0
 u, s, vh = np.linalg.svd(p0_mat, full_matrices=False)
 
 # Use only the first `r` singular values
-x0 = u[:, :tree.root.rankOut()]
-q = np.diag(s[:tree.root.rankOut()])
-x1 = vh[:tree.root.rankOut(), :].T
+x0 = u[:, : tree.root.rankOut()[0]]
+q = np.diag(s[: tree.root.rankOut()[0]])
+x1 = vh[: tree.root.rankOut()[0], :].T
 
 # SVD of x0
 if partition_str is partition[0]:
@@ -135,6 +158,7 @@ elif partition_str is partition[0]:
     x11_sum = np.sum(x11, axis=0)
     x0_sum = np.sum(x0, axis=0)
     x1_sum = np.array([x10_sum @ q1[:, :, i] @ x11_sum.T for i in range(r_out[0])])
+    # x1_sum = np.array([x10_sum @ q1[:, :, i] @ x11_sum.T for i in range(r_out[Id("1")])])
 else:
     initial_conditions.Q[0][:, :, 0] = q
     initial_conditions.Q[1][:] = q0
@@ -145,6 +169,7 @@ else:
     x00_sum = np.sum(x00, axis=0)
     x01_sum = np.sum(x01, axis=0)
     x0_sum = np.array([x00_sum @ q0[:, :, i] @ x01_sum.T for i in range(r_out[0])])
+    # x0_sum = np.array([x00_sum @ q0[:, :, i] @ x01_sum.T for i in range(r_out[Id("0")])])
     x1_sum = np.sum(x1, axis=0)
 
 norm = x0_sum @ q @ x1_sum.T
